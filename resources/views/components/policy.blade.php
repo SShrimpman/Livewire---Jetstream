5346169d51784d9f9bd6470337025647ea020a7f
António Camarão <camaraoacsn@gmail.com>
2023-10-02 10:14:45 +0100

    Episode 20 - Wire:navigate (SPA)

---
+++ resources/views/components/policy.blade.php
@@ -0,0 +1,75 @@
+<div class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 p-6 lg:p-8">
+    <div>
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Privacy Policy</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            This Privacy Policy outlines how we collect, use, disclose, and manage your personal information. By using our services, you agree to the practices described in this policy.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Information We Collect</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            We may collect personal information you provide directly, such as your name, email address, and other contact details. Additionally, we may collect usage data, including IP addresses, browser type, and device information.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">How We Use Your Information</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            We use your information to provide and improve our services, respond to inquiries, and communicate with you. We may also use aggregated, anonymized data for analytical purposes.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Disclosure of Information</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            We do not sell or rent your personal information to third parties. However, we may share information with trusted service providers who assist us in operating our website and conducting business.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Cookies and Analytics</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            We may use cookies and similar tracking technologies to enhance your experience on our website. These technologies help us analyze user behavior and improve our services.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Security</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            We take reasonable measures to protect your personal information from unauthorized access, disclosure, alteration, or destruction.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Your Rights</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            You have the right to access, correct, or delete your personal information. You may also request information about the data we hold about you.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Changes to this Policy</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            We may update this Privacy Policy periodically. The latest version will be posted on our website with the effective date.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Contact Us</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            If you have any questions or concerns about this Privacy Policy, please contact us at 19:00.
+        </p>
+    </div>
+</div>
---
+++ resources/views/components/terms.blade.php
@@ -0,0 +1,75 @@
+<div class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 p-6 lg:p-8">
+    <div>
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Terms of Responsibility</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            These Terms of Responsibility ("Terms") outline your obligations and responsibilities when using our services. By accessing or using our services, you agree to abide by these Terms.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">User Conduct</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            You agree to use our services responsibly and lawfully. You shall not engage in any behavior that violates applicable laws, infringes on intellectual property rights, or disrupts the functionality of our platform.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Accuracy of Information</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            You are responsible for providing accurate and up-to-date information when using our services. Any false or misleading information provided may result in the termination of your access.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Third-Party Links</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            Our services may contain links to third-party websites or services. We do not endorse or have control over these sites, and you access them at your own risk.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Limitation of Liability</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            We are not liable for any direct, indirect, incidental, or consequential damages arising out of your use or inability to use our services. This includes but is not limited to loss of data, profits, or business opportunities.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Indemnification</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            You agree to indemnify and hold us harmless from any claims, damages, or losses arising from your use of our services, violation of these Terms, or infringement of any third-party rights.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Termination</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            We reserve the right to suspend or terminate your access to our services at our discretion, without prior notice, if we believe you have violated these Terms.
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Governing Law</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            These Terms shall be governed by and construed in accordance with the laws of [Jurisdiction]. Any disputes arising from these Terms will be subject to the exclusive jurisdiction of the courts in [Jurisdiction].
+        </p>
+
+        <div class="flex items-center">
+            <h2 class="ml-3 text-xl font-semibold text-gray-900 dark:text-white">Changes to Terms</h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            We may update these Terms periodically. The latest version will be posted on our website with the effective date.
+        </p>
+    </div>
+</div>
